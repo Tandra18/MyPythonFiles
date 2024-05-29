8b0e5b3094bde324775bb7113c85851a217e1f78
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2024-05-29 23:52:45 +0630

    "new changed"

---
+++ array.py
@@ -0,0 +1,12 @@
+cars = ["volvo", "audi", "bmw"]
+x = len(cars)
+# print(cars[0])
+# cars[0] = "toyota"
+# print(cars[0])
+cars.append("honda")
+cars.remove("bmw")
+cars.pop(0)
+print(x)
+for y in cars:
+    print(y)
+
---
+++ lambda.py
@@ -0,0 +1,28 @@
+# x = lambda a : a + 10
+# print(x(5))
+
+# x = lambda a,b : a * b
+# print(x(5,6))
+
+# x = lambda a, b, c : (a + b) * c
+# print(x(1,2,3))
+
+# def myFunc(n):
+#     return lambda a : a * n
+# mydoubler = myFunc(2)
+# print(mydoubler(11))
+
+# def myFunc(n):
+#     return lambda a : a * n
+# myTripler = myFunc(3)
+# print(myTripler(12))
+
+# def myFunc(x):
+#     return lambda a : a * x
+# myDoubler = myFunc(2)
+# myTripler = myFunc(3)
+#
+# print(myDoubler(12))
+# print(myTripler(12))
+
+
